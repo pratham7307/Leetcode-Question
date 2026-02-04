1347b6bbde6b5487ce8dfaaac24ada8affbf8279
Pratham Kumar Rai <raipratham8957@gmail.com>
2026-02-04 22:55:34 +0530

    Time: 4 ms (73.27%) | Memory: 45.5 MB (18.72%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,21 +1,40 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // int[] ans = new int[nums1.length];
+        // for (int i = 0; i < nums1.length; i++) {
+        //     int nextGreater = -1;
+        //     for (int j = 0; j < nums2.length; j++) {
+        //         if (nums1[i] == nums2[j]) {
+        //             for (int k = j + 1; k < nums2.length; k++) {
+        //                 if (nums2[k] > nums2[j]) {
+        //                     nextGreater = nums2[k];
+        //                     break;
+        //                 }
+        //             }
+        //             break; 
+        //         }
+        //     }
+        //     ans[i] = nextGreater;
+        // }
+        // return ans;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && num > stack.peek()) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
+        }
         int[] ans = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            int nextGreater = -1;
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    for (int k = j + 1; k < nums2.length; k++) {
-                        if (nums2[k] > nums2[j]) {
-                            nextGreater = nums2[k];
-                            break;
-                        }
-                    }
-                    break; 
-                }
-            }
-            ans[i] = nextGreater;
+            ans[i] = map.get(nums1[i]);
         }
+
         return ans;
     }
 }
