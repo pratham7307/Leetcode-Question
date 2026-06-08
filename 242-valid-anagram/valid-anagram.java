1856d14be5eee7849dc190f75e9ac1d1570e46c5
Pratham Kumar Rai <raipratham8957@gmail.com>
2026-06-08 19:03:36 +0530

    Time: 3 ms (91.91%) | Memory: 43.9 MB (98.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,33 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        HashMap<Character,Integer> s1=new HashMap<>();
-        HashMap<Character,Integer> t1=new HashMap<>();
-        for(char c:s.toCharArray()){
-            s1.put(c,s1.getOrDefault(c,0)+1);
+        // if(s.length()!=t.length()) return false;
+        // HashMap<Character,Integer> s1=new HashMap<>();
+        // HashMap<Character,Integer> t1=new HashMap<>();
+        // for(char c:s.toCharArray()){
+        //     s1.put(c,s1.getOrDefault(c,0)+1);
+        // }
+        // for(char c:t.toCharArray()){
+        //     t1.put(c,t1.getOrDefault(c,0)+1);
+        // }
+        // if(s1.equals(t1)) return true;
+        // return false;
+
+        if(s.length()!=t.length()){
+            return false;
         }
-        for(char c:t.toCharArray()){
-            t1.put(c,t1.getOrDefault(c,0)+1);
+        int[] arr=new int[26];
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
         }
-        if(s1.equals(t1)) return true;
-        return false;
+        for( j=0;j<t.length();j++){
+            arr[t.charAt(j)-'a']--;
+        }
+        for(j=0;j<arr.length;j++){
+            if(arr[j]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
