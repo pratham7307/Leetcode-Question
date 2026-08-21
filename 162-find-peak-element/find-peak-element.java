ecb95cd43adf59513deb59659371b9d18db092a7
Pratham Kumar Rai <raipratham8957@gmail.com>
2026-08-22 01:05:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (57.46%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        if(n==1) return 0;
+        int low=0,high=n-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[mid+1]){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
